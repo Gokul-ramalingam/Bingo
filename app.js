186f0af96e0ec900e241342e98599503061e1968
Gokul <gokulramalingam1@gmail.com>
2019-09-09 01:15:02 +0530

    Update in timer functionality

--- app.js
+++ app.js
@@ -2,6 +2,43 @@ let storedCells = [];
 // Save references to some individual <td> nodes
 let tableElement = document.getElementById("tab");
 let count = 1;
+
+
+
+let h = document.getElementById('minutes')
+let m = document.getElementById('seconds');
+let s = document.getElementById('milliSeconds');
+let seconds = 0, minutes = 0, hours = 0;
+let t;
+
+function add() {
+seconds++;
+if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+    }
+}
+
+h.textContent = hours ? (hours > 9 ? hours : "0" + hours) : "00";
+m.textContent = minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00";
+s.textContent = seconds > 9 ? seconds : "0" + seconds;
+
+timer();
+}
+
+function timer() {
+t = setTimeout(add, 15);
+}
+
+
+
+
+
+
+
 $("#button1").click(function(){
     if(count === 1){
     storedCells.push(tableElement.rows[1].cells[0].innerText);
@@ -37,10 +74,15 @@ $("#button1").click(function(){
     else if(count === 2){
     document.getElementById("button1").innerHTML='New Game';
     count++;
+    clearTimeout(t);
     }
     else
     { 
     storedCells.length = 0;
+    h.textContent = "00";
+    m.textContent = "00";
+    s.textContent = "00";
+    seconds = 0; minutes = 0; hours = 0;
     document.getElementById("button1").innerHTML='Start'; 
     count = 1;
     }
@@ -54,33 +96,21 @@ Array.prototype.randomElement = function () {
 
 
 $("#shootButton").click(function(){
-    console.log(storedCells.randomElement());
+    let randomNumber = storedCells.randomElement()
+    if(randomNumber < 10)
+    $("#randomNumber").text("0"+randomNumber);
+    else
+    $("#randomNumber").text(randomNumber);
 })
 
-    let h = document.getElementById('minutes')
-    let m = document.getElementById('seconds');
-    let s = document.getElementById('milliSeconds');
-    let seconds = 0, minutes = 0, hours = 0;
-    let t;
 
-function add() {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
-    }
-    
-    h.textContent = hours ? (hours > 9 ? hours : "0" + hours) : "00";
-    m.textContent = minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00";
-    s.textContent = seconds > 9 ? seconds : "0" + seconds;
 
-    timer();
-}
 
-function timer() {
-    t = setTimeout(add, 10);
-}
+
+
+
+
+
+
+
+
